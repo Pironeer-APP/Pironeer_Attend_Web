0046fa4ffa11f7741d218c884e0a15501b3c2446
OH-GITAEK <dhrlxor1512@gmail.com>
2024-05-27 18:43:11 +0900

    #5 [feat] DB연결시 관리자 유저가 없을경우 생성하도록 설정

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const api = require('./api/routers/index');
+const express = require("express");
+const connectDB = require("./config/db");
+const api = require("./api/routers/index");
+const { createInitAdmin } = require("./api/controllers/userController");
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', api);
+app.use("/api", api);
 
-const { swaggerUi, specs } = require("./swagger/swagger")
+const { swaggerUi, specs } = require("./swagger/swagger");
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-connectDB().then(() => {
+connectDB()
+  .then(async () => {
+    await createInitAdmin();
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.error("Database connection failed", err);
     process.exit(1);
-});
+  });
