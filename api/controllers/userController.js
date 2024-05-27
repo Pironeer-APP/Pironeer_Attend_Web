3834faff4d34fbad20bb28fa0307b46f86834368
OH-GITAEK <dhrlxor1512@gmail.com>
2024-05-27 18:38:49 +0900

    #5 [feat] create function createInitAdmin

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,9 +1,9 @@
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // JWT 비밀키
-const JWT_SECRET = 'piro';
+const JWT_SECRET = "piro";
 
 // Create: 새로운 사용자를 생성합니다.
 exports.createUser = async (req, res) => {
@@ -44,7 +44,11 @@ exports.getUserById = async (req, res) => {
 exports.updateUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    const user = await User.findByIdAndUpdate(req.params.id, { username, email, password }, { new: true });
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      { username, email, password },
+      { new: true }
+    );
     if (!user) {
       return res.status(404).send({ message: "User not found" });
     }
@@ -71,12 +75,30 @@ exports.login = async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
-    if (!user || !await bcrypt.compare(password, user.password)) {
-      return res.status(401).send('비밀번호 틀림');
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).send("비밀번호 틀림");
     }
-    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
-    res.json({ message: 'Login successfully', token });
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1h" });
+    res.json({ message: "Login successfully", token });
   } catch (error) {
-    res.status(500).send('Login error');
+    res.status(500).send("Login error");
+  }
+};
+
+exports.createInitAdmin = async () => {
+  try {
+    const adminExists = await User.findOne({ isAdmin: true });
+    if (!adminExists) {
+      const admin = new User({
+        username: "admin",
+        password: "piroAdmin",
+        email: "pirogramming.official@gmail.com",
+        isAdmin: true,
+      });
+      await admin.save();
+      console.log("Admin user created");
+    }
+  } catch (error) {
+    console.error("Error creating admin user", error);
   }
 };
