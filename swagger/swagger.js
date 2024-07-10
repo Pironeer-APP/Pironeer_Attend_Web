7ea15a06ea5c6ce0cf38a81d36ed9e9101f7d3b4
이승찬 <wkdvnddl050701@gmail.com>
2024-07-10 14:02:01 +0900

    Update swagger.js

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000", // Adjust this to your server address
+        url: "https://piro-recruiting.top", // Adjust this to your server address
         description: "api server",
       },
     ],
