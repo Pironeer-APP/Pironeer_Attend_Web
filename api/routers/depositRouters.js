8f15d195685f2c2132ed93925c95fa2f1121a6d5
OH-GITAEK <dhrlxor1512@gmail.com>
2024-07-20 11:39:24 +0900

    [feat] create admin diposit routers

--- api/routers/depositRouters.js
+++ api/routers/depositRouters.js
@@ -13,4 +13,19 @@ router.post('/deposit/:userId/defend/use', authenticateToken, depositController.
 //보증금 새로고침
 router.post('/deposit/:userId/reload', authenticateToken, depositController.depositReload);
 
+//보증금 방어권 삭제
+router.post('/:userId/defend/delete', authenticateToken,adminMiddleware, depositController.dependDelete);
+
+//보증금 방어권 추가
+router.post('/:userId/defend/add', authenticateToken,adminMiddleware, depositController.dependAdd);
+
+//보증금 새로고침 (모든 유저)
+router.post('/:userId/reload/all', authenticateToken,adminMiddleware, depositController.depositReloadAll);
+
+//과제 정보 입력
+router.post('/:userId/assignment/insert', authenticateToken,adminMiddleware, depositController.AssignmentInsert);
+
+// 과제 정보 수정(과제 리스트 수정)
+router.post('/:userId/assignment/update', authenticateToken,adminMiddleware, depositController.AssignmentUpdate);
+
 module.exports = router;
