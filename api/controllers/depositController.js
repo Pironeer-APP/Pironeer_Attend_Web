133b536b2fa5ebf9da0be61652d579c845d4fbe9
cathy <cathy02473500@gmail.com>
2024-07-22 19:01:44 +0900

    [fix] deposit 모델 수정, 보증금 방어권 사용 api 수정

--- api/controllers/depositController.js
+++ api/controllers/depositController.js
@@ -97,7 +97,6 @@ exports.AssignmentInsert = async (req, res) => {
                   deductionDate: new Date(),
                   deductedAmount: -10000,
                   deductionDetail: `미흡 과제 : ${assignment}`,
-                  isDefended: false
               });
               deposit.updateDeposit(-10000);
               // 변경사항 저장
@@ -123,7 +122,6 @@ exports.AssignmentInsert = async (req, res) => {
                   deductionDate: new Date(),
                   deductedAmount: -20000,
                   deductionDetail: `미제출 과제 : ${assignment}`,
-                  isDefended: false
               });
               deposit.updateDeposit(-20000);
               // 변경사항 저장
@@ -185,7 +183,6 @@ exports.AssignmentUpdate = async (req, res) => {
               deductionDate: new Date(),
               deductedAmount: check ? -10000 : -20000,
               deductionDetail: `${check ? '미흡' : '미제출'} 과제: ${assignment}`,
-              isDefended: false
           });
           // 보증금 업데이트
           deposit.updateDeposit(check ? -10000 : -20000);
@@ -240,25 +237,28 @@ exports.useDefend = async(req, res)=>{
             return res.status(400).send({message : "보증금 내역이 존재하지 않습니다."});
         }
 
-        const defendList = deposit.defendList;
-        const availableDefend = defendList.find(defend => defend.status === false);
+        if(deposit.deposit === 80000){
+            return res.status(400).send({message : "차감 내역이 존재하지 않아 보증금 방어권 사용이 불가합니다."});
+        }
+
+        const availableDefend = deposit.defendList.find(defend => defend.status === false);
         if(!availableDefend){
             return res.status(400).send({message : "사용 가능한 보증금 방어권이 없습니다."});
         }
 
         availableDefend.status = true;
         availableDefend.usedDate = new Date();
+        
+        deposit.updateDeposit(+10000);
 
-        const sortedDeductionList = deposit.deductionList.sort((a, b) => b.deductedAmount - a.deductedAmount);
-        if(sortedDeductionList.length > 0){
-            deposit.deposit += sortedDeductionList[0].deductedAmount;
-            sortedDeductionList[0].isDefended = true;
-            await deposit.save();
-        }
-        else{
-            await deposit.save();
-            return res.status(200).send({message : "보증금 차감 내역이 존재하지 않습니다."});
-        }
+        deposit.deductionList.push({
+            deductionIdx: deposit.deductionList.length + 1,
+            deductionDate: new Date(),
+            deductedAmount: +10000,
+            deductionDetail: "보증금 방어권 사용"
+        });
+
+        await deposit.save();
 
         res.status(200).send({message: "보증금 방어권이 정상적으로 사용되었습니다", user, defendList});
     }catch(error){
