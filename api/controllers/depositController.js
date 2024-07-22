015651f918ae781afd9860e5d6d087de43bc9394
cathy <cathy02473500@gmail.com>
2024-07-22 18:40:01 +0900

    [fix] 새로고침 관련 코드 삭제

--- api/controllers/depositController.js
+++ api/controllers/depositController.js
@@ -66,28 +66,3 @@ exports.useDefend = async(req, res)=>{
         res.status(500).send({message : "보증금 방어권을 사용하는 도중 오류가 발생했습니다.", error});
     }
 };
-
-//새로고침 버튼
-exports.depositReload = async(req, res)=>{
-    try{
-        //새로고침 버튼을 누름 -> modifiedTime 갱신 -> 보증금 객체 반환
-        const { userId } = req.params;
-
-        const user = await User.findById(userId);
-        if(!user){
-            return res.status(404).send({message : "존재하지 않는 사용자입니다."});
-        }
-
-        const deposit = await Deposit.findOne({user: user._id})
-        if(!deposit){
-            return res.status(400).send({message : "보증금 내역이 존재하지 않습니다."});
-        }
-
-        deposit.modifiedTime = new Date();
-        await deposit.save();
-
-        res.status(200).send({message : "보증금 내역이 정상적으로 업데이트 되었습니다.", user, deposit});
-    } catch(error){
-        res.status(500).send({message : "최근 내역 로딩 중 오류가 발생하였습니다.", error});
-    }
-};
