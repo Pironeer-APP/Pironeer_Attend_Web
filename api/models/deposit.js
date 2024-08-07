0903998a16f7662f84debe56809a2c06a03da591
OH-GITAEK <dhrlxor1512@gmail.com>
2024-08-08 01:49:39 +0900

    refac : deposit객체가 없을경우 생성하고 있을경우 해당 객체를 반환하는 메서드 추가(#44)

--- api/models/deposit.js
+++ api/models/deposit.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const User = require('../models/user');
 
 const depositSchema = new mongoose.Schema({
     //유저 객체
@@ -74,6 +75,30 @@ depositSchema.pre('save', function(next){
     this.modifiedTime = Date.now;
     next();
 });
+
+//deposit모델이 중간에 생겨서 deposit객체가 없는 유저도 있기 때문에 있으면 조회 없으면 생성하도록 하기 위해 만든 메서드
+depositSchema.statics.getOrCreateDeposit = async (userId) => {
+    let deposit = await Deposit.findOne({ user: userId });
+    if (!deposit) {
+        // User 정보 가져오기
+        const user = await User.findById(userId);
+        if (!user) {
+            throw new Error('유저를 찾을 수 없습니다');
+        }
+  
+        // 새로운 Deposit 객체 생성
+        deposit = new Deposit({ 
+            user: userId, 
+            userName: user.username,  // User 정보에서 userName 가져오기
+            deposit: 80000, // 초기 보증금 금액 설정
+            assignmentList: [], 
+            defendList: [],
+            deductionList: [],
+            modifiedTime: new Date()
+        });
+    }
+    return deposit;
+  };
 
 const Deposit = mongoose.model('Deposit', depositSchema);
 
